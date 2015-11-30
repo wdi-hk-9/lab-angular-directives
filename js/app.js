9338a4bf5d4aa1a72674c30324ab68d27e50a00f
Charlotte Kwan <kwan.charlotte.cl@gmail.com>
2015-11-30 18:10:25 +0800

    with rating

--- js/app.js
+++ js/app.js
@@ -5,9 +5,28 @@ angular.module('happyapp', [])
 function HappyController($scope){
   $scope.allEmotion = [];
 
+  $scope.getAvg = function(){
+    var eArr =$scope.allEmotion.map(function(str){
+      if (str =='Happy'){
+        return 1;
+      } else if (str == 'OK'){
+        return 0;
+      } else { return -1};
+    })
+    var total = 0;
+    total = eArr.reduce(function(a,b){
+      return a+b
+    })
+    var avg = total / eArr.length;
+    if (avg>=0.5){
+      return 'Happy';
+    } else if ( avg >=0){
+      return 'OK';
+    } else {return 'Unhappy'};
+  }
+
   $scope.addNewEmotion = function (){
     $scope.allEmotion.push($scope.dailyemotion);
-    console.log ($scope.allEmotion);
     $scope.dailyemotion = '';
   }
 }
