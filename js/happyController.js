ab2f1d27ca8c34741e681469b9397f9347934607
adaau <ada.au.cheng@gmail.com>
2015-12-01 08:46:00 +0800

    completed basic app

---
+++ js/happyController.js
@@ -0,0 +1,25 @@
+angular.module("HappyApp")
+  .controller("HappyController", HappyController);
+
+function HappyController($scope) {
+  // $scope.faces = [
+  //   {face: "happy", src: "happy.png"},
+  //   {face: "neutral", src: "neutral.png"},
+  //   {face: "sad", src: "sad.png"}
+  // ];
+
+  $scope.allFaces = [
+    {feeling: "img/happy.png",    day: 1},
+    {feeling: "img/neutral.png",  day: 2},
+    {feeling: "img/sad.png",      day: 3},
+  ];
+
+
+  $scope.newFace = { feeling: "", day: "" };
+
+  var dayNumber = $scope.allFaces.length + 1;
+
+  $scope.addNewFace = function() {
+    $scope.allFaces.push({feeling: $scope.newFace.feeling, day: dayNumber++});
+  }
+}
