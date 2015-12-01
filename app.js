d4ccd5e7650c5b779b13239108a86dd29982ffae
Philippa Johnstone <pecj@hotmail.co.uk>
2015-12-01 23:32:15 +0800

    no average ratings working

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ angular.module("happyApp", [])
 
     $scope.allHappy = [];
 
-
-    $scope.newHappy = {
-      status : '',
-      date   : ''
-    }
+    // $scope.newHappy = {
+    //   status : '',
+    //   date   : ''
+    // }
 
     $scope.addHappy = function () {
       $scope.date = new Date ();
@@ -20,33 +19,34 @@ angular.module("happyApp", [])
 
       $scope.emoticonSelected = "";
 
+      console.log($scope.allHappy);
+      // angular.forEach(allHappy, function (){
+      //   console.log(allHappy.status);
+      // })
+
+    // $scope.getAverage = function () {
+    //   var averageArray = $scope.allHappy.map(function (string){
+    //     if (string == 'happy') {
+    //       return 1;
+    //     } else if (string == 'ok') {
+    //       return 0;
+    //     } else { return -1};
+    //   })
+
+    //   var total = 0;
+
+    //   total = averageArray.reduce (function (a,b) {
+    //     return a + b;
+    //   })
+
+    //   var avgerage = total/averageArray.length;
+
+    //     if (avgerage >= 0.5) {
+    //       return 'Happy';
+    //     } else if ( avgerage >= 0) {
+    //       return 'Ok';
+    //     } else { return 'Sad'};
+    // }
+
     };
-
-    console.log($scope.allHappy);
-
-    $scope.getAverage = function () {
-      var averageArray = $scope.allHappy.map(function (string){
-        if (string == 'happy') {
-          return 1;
-        } else if (string == 'ok') {
-          return 0;
-        } else { return -1};
-      })
-
-      var total = 0;
-
-      total = averageArray.reduce (function (a,b) {
-        return a + b;
-      })
-
-      var avgerage = total/averageArray.length;
-
-        if (avgerage >= 0.5) {
-          return 'Happy';
-        } else if ( avgerage >= 0) {
-          return 'Ok';
-        } else { return 'Sad'};
-
-    }
-
 });
